ead6c2f015c78666c53495c41912ff7a72ab131a
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-12-16 19:21:40 +0200

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,60 @@
+$(function(){
+	$('.button-burger').on('click', function(){
+		$('.navigation__list').addClass('active');
+		$('.button-burger').fadeOut();
+	});
+
+	$('.button-close, .navigation__list-item').on('click', function(){
+		$('.navigation__list').removeClass('active');
+		$('.button-burger').fadeIn();
+	});
+// ......................MixItUp2......................
+
+	$('#portfolio-body').mixItUp();
+
+// ......................Scroll........................
+
+	$('a[href^="#"]').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		},800);
+	});
+// ...................JQ Validation....................
+
+	$('.contact-form').validate({
+		rules: {
+			name: {required: true},
+			email: {required: true, email: true},
+			message: {required: true}
+		},
+		messages: {
+			name: 'Пожалуйста, введите свое имя',
+			email: {
+				required: 'Поле E-mail обязательное для заполнения',
+				email: 'Введите пожалуйста корректный e-mail'
+			},
+			message: 'Пожалуйста, введите текст сообщения'
+		},
+		errorClass: "error",
+		validClass: "valid"
+	});
+
+//...................E-mail Ajax Send...................
+
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо! Ваше сообщение отправлено");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+});
